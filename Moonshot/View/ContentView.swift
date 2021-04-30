d98416bb19a3039eab14f064d1d6479832b0b6f4
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-30 09:24:34 -0400

    finished day 40

---
+++ Moonshot/View/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Antonio Vega on 4/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 44, height: 44)
+                        
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                            Text(mission.formattedLaunchDate)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
